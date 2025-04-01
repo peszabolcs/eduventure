cd8207ae96c59b608bf606c9f267939f1d1023ec
peszabolcs <perjesidev@gmail.com>
2025-04-01 21:17:21 +0200

    option to go back to the latest question

--- src/components/career/CareerOrientationModule.jsx
+++ src/components/career/CareerOrientationModule.jsx
@@ -48,6 +48,14 @@ const CareerOrientationModule = () => {
     }
   };
 
+  const handleBack = () => {
+    if (currentQuestion > 0) {
+      setCurrentQuestion(currentQuestion - 1);
+      // Remove the last answer when going back
+      setAnswers(answers.slice(0, -1));
+    }
+  };
+
   const handleFindExpert = () => {
     navigate("/szakerto-kereso");
   };
@@ -326,6 +334,30 @@ const CareerOrientationModule = () => {
                 </motion.button>
               ))}
             </div>
+
+            {/* Back button */}
+            {currentQuestion > 0 && (
+              <motion.button
+                onClick={handleBack}
+                className="mt-6 px-6 py-2 bg-purple-500/20 text-purple-300 rounded-lg hover:bg-purple-500/30 transition-all duration-300 flex items-center gap-2"
+                whileHover={{ scale: 1.02 }}
+                whileTap={{ scale: 0.98 }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Vissza
+              </motion.button>
+            )}
           </motion.div>
         </div>
       </div>
